c31296386cd99448209034d272ea24b93251ba1b
Architect38 <gothic38101@gmail.com>
2019-11-27 23:06:16 +0400

    Updates

--- precache-manifest.320f439adf56db44c08dbd1dbe9b3624.js
+++ precache-manifest.320f439adf56db44c08dbd1dbe9b3624.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "af6342e5519e769414f136cc1f316b0a",
+    "revision": "6030f6ca1f645ef437e56efe9cd21985",
     "url": "/React-Tetris/index.html"
   },
   {
-    "revision": "133f083e19e3a1959478",
+    "revision": "c3e7c9c135560d097ce9",
     "url": "/React-Tetris/static/css/main.29396526.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/React-Tetris/static/js/2.25e5f53a.chunk.js"
   },
   {
-    "revision": "133f083e19e3a1959478",
-    "url": "/React-Tetris/static/js/main.bff5513c.chunk.js"
+    "revision": "c3e7c9c135560d097ce9",
+    "url": "/React-Tetris/static/js/main.527f6f43.chunk.js"
   },
   {
     "revision": "88795f8def2f87aef32e",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/React-Tetris/precache-manifest.4f5726dc961485f7648213bacf1e3f7c.js"
+  "/React-Tetris/precache-manifest.320f439adf56db44c08dbd1dbe9b3624.js"
 );
 
 self.addEventListener('message', (event) => {
