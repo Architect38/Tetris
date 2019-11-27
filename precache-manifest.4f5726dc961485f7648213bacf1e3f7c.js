0de2c7fac0ddeb474e581ff1d2548f8d822ce258
Architect38 <gothic38101@gmail.com>
2019-11-27 23:03:16 +0400

    Updates

---
+++ precache-manifest.4f5726dc961485f7648213bacf1e3f7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af6342e5519e769414f136cc1f316b0a",
+    "url": "/React-Tetris/index.html"
+  },
+  {
+    "revision": "133f083e19e3a1959478",
+    "url": "/React-Tetris/static/css/main.29396526.chunk.css"
+  },
+  {
+    "revision": "2d2a4ee8606366519ec4",
+    "url": "/React-Tetris/static/js/2.25e5f53a.chunk.js"
+  },
+  {
+    "revision": "133f083e19e3a1959478",
+    "url": "/React-Tetris/static/js/main.bff5513c.chunk.js"
+  },
+  {
+    "revision": "88795f8def2f87aef32e",
+    "url": "/React-Tetris/static/js/runtime-main.ade3373d.js"
+  }
+]);
